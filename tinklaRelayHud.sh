764c059afc91e0cf20d303ebc45b71012a4ab65a
BogGyver <bogdan.butoi@gmail.com>
2024-02-15 01:22:34 -0500

    add touch to start script

--- tinklaRelayHud.sh
+++ tinklaRelayHud.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sudo systemctl isolate multi-user.target
 export QT_QPA_PLATFORM=linuxfb
+export QT_QPA_FB_TSLIB=1
+export TSLIB_TSDEVICE=/dev/input/mice
 /home/ubuntu/tinklaRelayHUD/tinklaRelayHUD /sys/class/backlight/rpi_backlight/brightness
